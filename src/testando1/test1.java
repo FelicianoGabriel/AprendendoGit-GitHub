2a50f74df4fb6bfb1ba953b00314d54015dea4ba
Feliciano Gabriel <f.gmatospereira@gmail.com>
2025-02-26 18:10:15 -0300

    commitzinho

--- src/testando1/test1.java
+++ src/testando1/test1.java
@@ -3,5 +3,6 @@ package testando1;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Aqui vai uma mensgaem");
+        System.out.println("1");
     }
 }
